3fa1203f94c13c7bf5f3ced08b4f5992ec8e6a45
Aias-maker <tomidsf11@gmail.com>
2019-09-08 21:10:03 -0300

    Project v.2

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 let mongoose = require('./../config/conexion');
 let Persona = require('./../models/persona');
 
-/* GET home page. */
+//rellenar con lo que tengo en mongo
 router.get('/', function(req, res, next) {
   Persona.find((err,personas)=>{
 
@@ -14,11 +14,31 @@ router.get('/', function(req, res, next) {
   });
 });
 
+//insertar
 router.get('/persona/nuevo', function(req, res, next){
  
   res.render('personaForm',{});
 });
 
+//modificar
+router.get('/persona/modificar/:id', (req,res,next)=>{
+  let idreq = req.params.id;
+
+  Persona.findOne({_id: idreq}, (err,persona)=>{
+    if(err) throw err;
+    res.render('personaForm', {persona:persona});
+  });
+});
+
+//eliminar
+router.get('/persona/eliminar/:id', (req,res,next)=>{
+  let idreq = req.params.id;
+
+  Persona.remove({_id: idreq}, (err)=>{
+    if(err) throw err;
+    res.redirect('/');
+  });
+});
 
 
 
